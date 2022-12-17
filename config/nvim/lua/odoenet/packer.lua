ce0e69eb68f4f342f5911cce8658a761bb5a711b
odoe <odoe@odoe.net>
2022-12-17 12:29:13 -0800

    feat: vim-commentary

--- config/nvim/lua/odoenet/packer.lua
+++ config/nvim/lua/odoenet/packer.lua
@@ -29,6 +29,7 @@ return require('packer').startup(function(use)
 	use('mbbill/undotree')
 
 	use('tpope/vim-fugitive')
+	use('tpope/vim-commentary')
 
     use {
         'VonHeikemen/lsp-zero.nvim',
--- config/nvim/plugin/packer_compiled.lua
+++ config/nvim/plugin/packer_compiled.lua
@@ -185,6 +185,11 @@ _G.packer_plugins = {
     path = "/home/odoe/.local/share/nvim/site/pack/packer/start/undotree",
     url = "https://github.com/mbbill/undotree"
   },
+  ["vim-commentary"] = {
+    loaded = true,
+    path = "/home/odoe/.local/share/nvim/site/pack/packer/start/vim-commentary",
+    url = "https://github.com/tpope/vim-commentary"
+  },
   ["vim-fugitive"] = {
     loaded = true,
     path = "/home/odoe/.local/share/nvim/site/pack/packer/start/vim-fugitive",
