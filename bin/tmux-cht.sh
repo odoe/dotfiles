e8a787be627647cc24999193c7b5f2a8b6b86179
odoe <odoe@odoe.net>
2022-12-30 15:41:48 -0800

    feat: I think scripts are working now

--- bin/tmux-cht.sh
+++ bin/tmux-cht.sh
@@ -4,10 +4,10 @@ languages=$(echo "golang c cpp javascript typescript rust elixir" | tr " " "\n")
 core_utils=$(echo "find xargs sed awk tar" | tr " " "\n")
 selected=$(echo -e "$languages\n$core_utils" | fzf)
 
-read -p "QUERY: " query
+read -p "Query: " query
 
 if echo "$languages" | grep -qs $selected; then
-    tmux split-window -h bash -c "curl cht.sh/$selected/`echo "$query" | tr " " "+"` | bat & while [ : ]; do sleep 1; done"
+    tmux split-window -p 100 -h bash -c "curl cht.sh/$selected/`echo "$query" | tr " " "+"` & while [ : ]; do sleep 1; done"
 else
-    tmux split-window -h bash -c "curl cht.sh/$selected~$query | bat & while [ : ]; do sleep 1; done"
+    tmux split-window -p 100 -h bash -c "curl cht.sh/$selected~$query | less & while [ : ]; do sleep 1; done"
 fi
