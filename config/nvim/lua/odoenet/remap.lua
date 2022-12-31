e96ecb92bb8a5c07b6c30939321dd58bffc51eba
odoe <odoe@odoe.net>
2022-12-30 20:25:37 -0800

    chore: cleanup

--- config/nvim/lua/odoenet/remap.lua
+++ config/nvim/lua/odoenet/remap.lua
@@ -45,4 +45,3 @@ vim.keymap.set("n", "<C-k>", "<C-w>k", opts)
 vim.keymap.set("n", "<C-h>", "<C-w>h", opts)
 vim.keymap.set("n", "<C-j>", "<C-w>j", opts)
 vim.keymap.set("n", "<C-l>", "<C-l>h", opts)
-
