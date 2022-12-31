106c547c6ac02a14afca57fec6283f68b0c1fcce
odoe <odoe@odoe.net>
2022-12-30 19:43:22 -0800

    feat: lazygit and floaterm

--- config/nvim/lua/odoenet/packer.lua
+++ config/nvim/lua/odoenet/packer.lua
@@ -31,7 +31,8 @@ return require('packer').startup(function(use)
     use('theprimeagen/harpoon')
     use('mbbill/undotree')
 
-    use('tpope/vim-fugitive')
+    -- use('tpope/vim-fugitive')
+    use('voldikss/vim-floaterm')
     use('tpope/vim-commentary')
 
     -- use {
