d6ecf428c11943a82ead1d3ea0a641e9b4492889
odoe <odoe@odoe.net>
2023-03-27 17:07:09 -0700

    meh

--- nvim/.config/nvim/lua/odoenet/plugins.lua
+++ nvim/.config/nvim/lua/odoenet/plugins.lua
@@ -71,9 +71,7 @@ local plugins = {
             { 'hrsh7th/nvim-cmp' },
             { 'hrsh7th/cmp-buffer' },
             { 'hrsh7th/cmp-path' },
-            { 'saadparwaiz1/cmp_luasnip',
-                { show_autosnippets = true }
-            },
+            { 'saadparwaiz1/cmp_luasnip' },
             { 'hrsh7th/cmp-nvim-lsp' },
             { 'hrsh7th/cmp-nvim-lua' },
 
