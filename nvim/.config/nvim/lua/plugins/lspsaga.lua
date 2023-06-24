c62e2a0e2ddf4668780769905baeaea75bda729c
Rene Rubalcava <rrubalcava@esri.com>
2023-06-23 19:41:14 -0700

    feat(nvim): disable some stuff, add lspsaga

---
+++ nvim/.config/nvim/lua/plugins/lspsaga.lua
@@ -0,0 +1,13 @@
+return {
+  "glepnir/lspsaga.nvim",
+  event = "LspAttach",
+  config = function()
+    require("lspsaga").setup({})
+    require("config.lspsaga")
+  end,
+  dependencies = {
+    { "nvim-tree/nvim-web-devicons" },
+    --Please make sure you install markdown and markdown_inline parser
+    { "nvim-treesitter/nvim-treesitter" },
+  },
+}
--- nvim/.config/nvim/lua/plugins/misc.lua
+++ nvim/.config/nvim/lua/plugins/misc.lua
@@ -1,5 +1,6 @@
 return {
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "folke/noice.nvim", enabled = false },
   { "nvim-treesitter/nvim-treesitter-context" },
   { "voldikss/vim-floaterm" },
   {
