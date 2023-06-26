9e81b5ba12dd0cc70f04056861d563835c0504f6
Rene Rubalcava <rrubalcava@esri.com>
2023-06-26 15:39:41 -0700

    fix(wezterm): update opacity

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This table will hold the configuration.
 local config = {}
@@ -7,21 +7,20 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
 
-config.font = wezterm.font 'Hack Nerd Font'
+config.font = wezterm.font("Hack Nerd Font")
 config.font_size = 12.0
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Hardcore'
+config.color_scheme = "Hardcore"
 
-config.window_background_opacity = 0.7
+config.window_background_opacity = 0.95
 
 config.enable_tab_bar = false
 
 -- and finally, return the configuration to wezterm
 return config
-
