3aa422fd215ea1c6c9f42edc2c5b91288e7d437d
Rene Rubalcava <rrubalcava@esri.com>
2023-06-30 13:37:21 -0700

    chore(nvim): disable bufferline tabs

--- nvim/.config/nvim/lua/plugins/misc.lua
+++ nvim/.config/nvim/lua/plugins/misc.lua
@@ -1,6 +1,7 @@
 return {
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "folke/noice.nvim", enabled = false },
+  { "akinsho/bufferline.nvim", enabled = false },
   { "nvim-treesitter/nvim-treesitter-context" },
   { "voldikss/vim-floaterm" },
   {
