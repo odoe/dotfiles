eea822fce1916194d7423caa882b8a58beecb615
Rene Rubalcava <rrubalcava@esri.com>
2024-11-26 10:18:39 -0800

    chore: fix some markdown

--- nvim/.config/nvim/lua/custom/plugins/markdown.lua
+++ nvim/.config/nvim/lua/custom/plugins/markdown.lua
@@ -1,12 +1,13 @@
 return {
   {
     'MeanderingProgrammer/render-markdown.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
     opts = {},
+    ft = { 'markdown' },
   },
   {
     'iamcco/markdown-preview.nvim',
