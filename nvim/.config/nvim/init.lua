c18be4c9115495d39477617719aa5e7ac0b118e2
Rene Rubalcava <rrubalcava@esri.com>
2024-12-02 16:08:43 -0800

    chore(nvim): diagnostics

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1056,6 +1056,7 @@ vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.s
 })
 
 vim.diagnostic.config {
+  virtual_text = false,
   float = { border = _border },
   signs = {
     text = {
