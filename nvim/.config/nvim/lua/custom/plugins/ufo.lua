6901e61e20e15c7229e8b3d7e276760ff9289a34
Rene Rubalcava <rrubalcava@esri.com>
2025-01-29 08:03:49 -0800

    chore: nvim - ufo

---
+++ nvim/.config/nvim/lua/custom/plugins/ufo.lua
@@ -0,0 +1,17 @@
+return {
+  'kevinhwang91/nvim-ufo',
+  dependencies = { 'kevinhwang91/promise-async' },
+  config = function()
+    vim.o.foldcolumn = '1'
+    vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+    vim.o.foldlevelstart = 99
+    vim.o.foldenable = true
+
+    local ufo = require 'ufo'
+    ufo.setup {
+      provider_selector = function(bufnr, filetype, buftype)
+        return { 'treesitter', 'indent' }
+      end,
+    }
+  end,
+}
