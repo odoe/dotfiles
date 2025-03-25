57bc31dede8befef45b1e1da55273346428d2941
Rene Rubalcava <rrubalcava@esri.com>
2025-03-25 08:23:08 -0700

    chore(nvim): debug

--- nvim/.config/nvim/lua/kickstart/plugins/debug.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/debug.lua
@@ -23,6 +23,12 @@ return {
 
     -- Add your own debuggers here
     'leoluz/nvim-dap-go',
+    'mxsdev/nvim-dap-vscode-js',
+    {
+      'microsoft/vscode-js-debug',
+        opt = true,
+        run = 'npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out'
+    }
   },
   config = function()
     local dap = require 'dap'
@@ -44,6 +50,84 @@ return {
         'delve',
       },
     }
+
+    -- JavaScript/TypeScript/Node
+
+    -- setup adapters
+require('dap-vscode-js').setup({
+  debugger_path = vim.fn.stdpath('data') .. '/mason/packages/js-debug-adapter',
+  debugger_cmd = { 'js-debug-adapter' },
+  adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' },
+})
+
+-- custom adapter for running tasks before starting debug
+local custom_adapter = 'pwa-node-custom'
+dap.adapters[custom_adapter] = function(cb, config)
+  if config.preLaunchTask then
+      local async = require('plenary.async')
+      local notify = require('notify').async
+
+      async.run(function()
+          ---@diagnostic disable-next-line: missing-parameter
+          notify('Running [' .. config.preLaunchTask .. ']').events.close()
+      end, function()
+          vim.fn.system(config.preLaunchTask)
+          config.type = 'pwa-node'
+          dap.run(config)
+      end)
+  end
+end
+
+-- language config
+for _, language in ipairs({ 'typescript', 'typescriptreact', 'javascript', 'svelte', 'vue' }) do
+  dap.configurations[language] = {
+      {
+          name = 'Launch',
+          type = 'pwa-node',
+          request = 'launch',
+          program = '${file}',
+          rootPath = '${workspaceFolder}',
+          cwd = '${workspaceFolder}',
+          sourceMaps = true,
+          skipFiles = { '<node_internals>/**' },
+          protocol = 'inspector',
+          console = 'integratedTerminal',
+      },
+      {
+          name = 'Attach to node process',
+          type = 'pwa-node',
+          request = 'attach',
+          rootPath = '${workspaceFolder}',
+          processId = require('dap.utils').pick_process,
+      },
+      {
+        -- use nvim-dap-vscode-js's pwa-chrome debug adapter
+        type = 'pwa-chrome',
+        request = 'attach',
+        -- processId = require 'dap.utils'.pick_process,
+        -- name of the debug action
+        name = 'Launch Chrome to debug client side code',
+        -- default vite dev server url
+        url = 'http://localhost:5173',
+        -- for TypeScript/Svelte
+        -- sourceMaps = true,
+        webRoot = '${workspaceFolder}/src',
+        -- protocol = 'inspector',
+        -- port = 9222,
+        -- skip files from vite's hmr
+        skipFiles = { '**/node_modules/**/*', '**/@vite/*', '**/src/client/*', '**/src/*' },
+      },
+      {
+        type = 'pwa-chrome',
+        reques = 'attach',
+        port = 9222,
+        name = 'Attach Chrome',
+        url = 'http://localhost:5173/',
+        webRoot = '${workspaceFolder}'
+      }
+  }
+end
+   
 
     -- Basic debugging keymaps, feel free to change to your liking!
     vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
