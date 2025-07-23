99b6e6b0cd1749c9fcb6a8cc59c3cee7a87f86e5
Rene Rubalcava <rrubalcava@esri.com>
2025-07-23 13:10:16 -0700

    feat(nvim): codecompanion keys

--- nvim/.config/nvim/lua/custom/plugins/codecampanion.lua
+++ nvim/.config/nvim/lua/custom/plugins/codecampanion.lua
@@ -5,4 +5,36 @@ return {
     'nvim-lua/plenary.nvim',
     'nvim-treesitter/nvim-treesitter',
   },
+  keys = {
+    {
+      '<leader>aa',
+      mode = { 'n', 'v' },
+      '<cmd>CodeCompanionChat Toggle<cr>',
+      desc = 'CodeCompanion Chat',
+    },
+    {
+      '<leader>aq',
+      ':CodeCompanion',
+      mode = { 'n', 'x', 'v' },
+      desc = 'CodeCompanion inline chat',
+    },
+    {
+      '<leader>AA',
+      '<cmd>CodeCompanionActions<cr>',
+      mode = { 'n', 'v' },
+      desc = 'CodeCompanion actions',
+    },
+    {
+      '<leader>an',
+      '<cmd>CodeCompanionChat<cr>',
+      mode = { 'n', 'v' },
+      desc = 'CodeCompanion new chat',
+    },
+    {
+      '<leader>av',
+      '<cmd>CodeCompanionChat Add<cr>',
+      mode = { 'n', 'v' },
+      desc = 'send visual selection to chat',
+    },
+  },
 }
