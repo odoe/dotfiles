95a32fe240df38ab67c39a5cc742251fb335313a
Rene Rubalcava <rrubalcava@esri.com>
2026-01-20 10:10:33 -0800

    chore: updates

--- nvim/.config/nvim/lua/custom/plugins/snacks.lua
+++ nvim/.config/nvim/lua/custom/plugins/snacks.lua
@@ -48,7 +48,7 @@ return {
     { "<c-_>",      function() Snacks.terminal() end, desc = "which_key_ignore" },
     { "]]",         function() Snacks.words.jump(vim.v.count1) end, desc = "Next Reference", mode = { "n", "t" } },
     { "[[",         function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference", mode = { "n", "t" } },
-    -- { '<C-x>', function() Snacks.explorer.reveal() end },
+    { '<C-x>', function() Snacks.explorer.reveal() end },
     {
       "<leader>N",
       desc = "Neovim News",
